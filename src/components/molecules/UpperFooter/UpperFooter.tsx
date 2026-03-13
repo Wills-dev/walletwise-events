3026eba8a61a3530631816f07851b59584707644
victor <ejioguvictor6@gmail.com>
2026-03-13 21:02:38 +0100

    update: app links

--- src/components/molecules/UpperFooter/UpperFooter.tsx
+++ src/components/molecules/UpperFooter/UpperFooter.tsx
@@ -5,8 +5,10 @@ const UpperFooter = () => {
   return (
     <div className="flex justify-center flex-wrap gap-4">
       <FooterCardWrapper className="bg-white/5 border-white/25">
-        <div className="p-4">
-          <p className="text-[#A0A0A0] font-semibold text-sm">SPONSORSHIP BY</p>
+        <div className="p-4 space-y-2">
+          <p className="text-[#A0A0A0] font-semibold sm:text-sm text-xs">
+            SPONSORSHIP BY
+          </p>
           <div className="flex items-center gap-2">
             <Image
               src="/logo.svg"
@@ -15,19 +17,23 @@ const UpperFooter = () => {
               height={38.72}
               className="object-contain"
             />
-            <p className="text-white sm:text-[29.86px] text-xl font-semibold">
+            <p className="text-white sm:text-[29.86px] text-lg font-semibold">
               WalletWise
             </p>
           </div>
         </div>
       </FooterCardWrapper>
       <FooterCardWrapper>
-        <div className="p-4 space-y">
-          <p className="text-[#A0A0A0] font-semibold text-sm text-center">
+        <div className="p-4 space-y-2">
+          <p className="text-[#A0A0A0] font-semibold sm:text-sm text-xs text-center">
             DOWNLOAD WALLETWISE
           </p>
-          <div className="flex items-center gap-2">
-            <a href="http://" target="_blank" rel="noopener noreferrer">
+          <div className="flex items-center justify-center flex-wrap gap-2">
+            <a
+              href="https://apps.apple.com/us/app/walletwise-seamless-payment/id6744754064"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <Image
                 src="/assets/images/appStore.svg"
                 alt=""
@@ -36,7 +42,11 @@ const UpperFooter = () => {
                 className="object-contain"
               />
             </a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://play.google.com/store/apps/details?id=com.finance.walletwise.app&pcampaignid=web_share"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <Image
                 src="/assets/images/google.svg"
                 alt=""
@@ -49,21 +59,21 @@ const UpperFooter = () => {
         </div>
       </FooterCardWrapper>
       <FooterCardWrapper className="border-white/25 bg-white/10">
-        <div className="py-[20.2px] px-[33.67px]">
-          <p className="text-sm text-[#A0A0A0] ">
+        <div className="py-[20.2px] px-[33.67px] space-y-2">
+          <p className="sm:text-sm text-xs text-[#A0A0A0] ">
             PHYSICAL TICKETS AVAILABLE @
           </p>
-          <p className="font-semibold text-[19.33px] text-white">
+          <p className="font-semibold sm:text-[19.33px]  text-white">
             ARMSTRONG PAINT OSOGBO
           </p>
         </div>
       </FooterCardWrapper>
       <FooterCardWrapper>
         <div className="py-4 px-6">
-          <p className="text-[#A0A0A0] font-semibold max-sm:text-sm">
+          <p className="text-[#A0A0A0] font-semibold max-sm:text-sm text-xs">
             FOR SPONSORSHIP & ENQUIRES
           </p>
-          <p className="sm:text-[32.52px] text-[25px] font-bold text-white">
+          <p className="sm:text-[32.52px] text-lg font-bold text-white">
             +234 706 803 8186
           </p>
         </div>
